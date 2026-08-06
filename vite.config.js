e023408106225dbcd482349e7ef4c8b3849fe186
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2026-08-06 23:30:06 +0500

    fix: reset base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ Change 'ecommerce-frontend' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce-frontend/',
+  base: '/',
 })
